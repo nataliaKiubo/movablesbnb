97f36d7113afc0bf97c5520a5b27670bd6794c16
Natalia Ramirez <natalia.ramirez@hotmail.com>
2022-08-23 10:46:55 +0200

    added the validations for the movable model

--- app/models/movable.rb
+++ app/models/movable.rb
@@ -1,3 +1,6 @@
 class Movable < ApplicationRecord
   belongs_to :user
+  validates :name, :description, :min_rent_time, :max_rent_time, :main_image, :stock, :price, :categories, presence: true
+  validates :name, length: { in: 6..250 }, uniqueness: true
+  validates :categories, inclusion: { in: %w(Armchairs Sofas Chairs Tables Lights Rugs Beds) }
 end
