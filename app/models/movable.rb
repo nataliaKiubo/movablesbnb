8b97047c399bf31f4f1a4f200a8c0e523c184ab6
Natalia Ramirez <natalia.ramirez@hotmail.com>
2022-08-23 12:12:30 +0200

    removed the unknow method

--- app/models/movable.rb
+++ app/models/movable.rb
@@ -1,7 +1,6 @@
 class Movable < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_attachment :photo
   validates :name, :description, :min_rent_time, :max_rent_time, :main_image, :stock, :price, :categories, presence: true
   validates :name, length: { in: 6..250 }, uniqueness: true
   validates :categories, inclusion: { in: %w(Armchairs Sofas Chairs Tables Lights Rugs Beds) }
