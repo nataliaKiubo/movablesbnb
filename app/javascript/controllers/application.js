3cf29b4f84cf0a446e73316194ff4b24be512939
philippine von rochow <philippine.vonrochow@sciencespo.fr>
2022-08-25 10:26:40 +0200

    booking model

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,23 @@
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
 const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 // Configure Stimulus development experience
+
 application.debug = false
 window.Stimulus   = application
+
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
 
 export { application }
