40177cd4f7bef4131b21eec04a0be756e8744a67
welton torres <weltonmail@gmail.com>
2022-08-25 11:56:45 +0200

    added geocode basics structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_many :movables, dependent: :destroy # from the provider perspective
   has_many :bookings # from the renter perspective
-  has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
